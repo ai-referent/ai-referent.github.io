d97cef64ceb086eee5f0aecbd53b05c1eb344c16
ai-referent <ai-referent@users.noreply.github.com>
2026-03-12 18:14:59 +0000

    deploy: 50d9208e0e9e7ca1009c377a533dac34ace51eda

---
+++ _astro/hoisted.CbqHWJpG.js
@@ -0,0 +1,1 @@
+document.querySelectorAll(".nav-links a").forEach(e=>{e.addEventListener("click",t=>{e.getAttribute("href")?.startsWith("/#")?setTimeout(()=>{document.querySelector(".nav-links")?.classList.remove("open")},100):document.querySelector(".nav-links")?.classList.remove("open")})});
